5361afbe7539c736970ee4789b93c640a8ebbc7f
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-01 11:54:40 +0530

    create signup container

--- frontend/src/pages/Auth/Signup.jsx
+++ frontend/src/pages/Auth/Signup.jsx
@@ -67,6 +67,15 @@ const Signup = () => {
         password: "",
         username: "",
       });
-    }
+    };
+
+    return(
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      {/* Signup card container */}
+      <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
+        <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">
+          Create Your Account
+        </h2>
+    )
 
 }  
