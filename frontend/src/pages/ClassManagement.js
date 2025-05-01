57bd34c3f697f5d7d5f204be511958c7e14ae582
Sachinthika Jayawardhana <sachiindunil2020@gmail.com>
2025-05-01 20:31:29 +0530

    change

--- frontend/src/pages/ClassManagement.js
+++ frontend/src/pages/ClassManagement.js
@@ -129,7 +129,7 @@ function ClassManagement() {
                   Create New Class
                 </h1>
                 <p className="mt-1 text-sm text-gray-600">
-                  Add a new class to your Smart School Management system
+                  Add a new class to your Smart School Management System
                 </p>
               </div>
               <div className="mt-4 md:mt-0 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-3">
