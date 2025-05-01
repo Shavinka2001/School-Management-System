520b3405978272d4cf0b9ed011d5b126f1e33853
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-01 10:57:24 +0530

    import necessary modules in AuthRoutes.js

---
+++ backend/routes/AuthRoute.js
@@ -0,0 +1,3 @@
+// Import necessary modules
+import express from "express"; // Using ESM import
+import { Signup, Login, userVerification } from "../controllers/AuthController.js"; // Correct import with ESM
