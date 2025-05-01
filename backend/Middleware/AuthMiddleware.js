4515a60be7cfcf607ae05eba3567efc622758e56
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-01 22:29:41 +0530

    update the authmiddleware .js

--- backend/Middleware/AuthMiddleware.js
+++ backend/Middleware/AuthMiddleware.js
@@ -1,19 +1,16 @@
-const User = require("../models/UserModel");
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 
-module.exports.userVerification = (req, res) => {
-  const token = req.cookies.token
-  if (!token) {
-    return res.json({ status: false })
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).json({ message: "Access Denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(400).json({ message: "Invalid Token" });
   }
-  jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
-    if (err) {
-     return res.json({ status: false })
-    } else {
-      const user = await User.findById(data.id)
-      if (user) return res.json({ status: true, user: user.username })
-      else return res.json({ status: false })
-    }
-  })
-}
+};
+
+export default authMiddleware;
