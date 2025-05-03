c03677db2579b24b7d16ed245992f36e1f87ada8
Sankadeepa_IM <imashsankadeepa27@gmail.com>
2025-05-03 17:18:48 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
-const { Configuration, OpenAIApi } = require('openai');
+
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const app = express();
